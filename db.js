32db2097cf5194c0499cbc3bb3cba530dd819708
eladiorios16-hash <eladiorios16@gmail.com>
2026-02-16 16:45:00 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,15 +1,18 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  // Forzamos los valores internos confirmados en tus capturas
-  host: 'mysql.railway.internal',
+  host: process.env.MYSQLHOST || 'mysql.railway.internal',
   port: 3306,
   user: 'root',
   password: 'gbDeOMOSothCZuATgwzgGIHLEALTdcvW',
   database: 'railway',
   waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  connectionLimit: 5,
+  queueLimit: 0,
+  // ESTO ES LO QUE SOLUCIONA EL "ACCESS DENIED" EN REDES IPv6 INTERNAS
+  authPlugins: {
+    mysql_clear_password: () => () => Buffer.from('gbDeOMOSothCZuATgwzgGIHLEALTdcvW')
+  }
 });
 
 module.exports = pool.promise();
