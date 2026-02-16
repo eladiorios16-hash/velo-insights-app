0174de9667feb9291dfadb6d7994ecf8610cf895
eladiorios16-hash <eladiorios16@gmail.com>
2026-02-16 11:27:27 +0100

    Update db.js

--- db.js
+++ db.js
@@ -146,7 +146,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('*', (req, res) => { res.sendFile(path.join(__dirname, 'public', 'index.html')); });
 
 // INICIO DEL SERVIDOR
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, '0.0.0.0', () => { 
     console.log(`🚀 Servidor Velo listo en puerto ${PORT}`); 
 });
